8ac84d46a93d3e24c9d2499e5bfabda7593729e4
Miki004 <162804895+Miki004@users.noreply.github.com>
2024-08-25 11:08:01 +0200

    Scena di errore

---
+++ src/main/java/com/example/clientfx/Controllers/ErrorWindow.java
@@ -0,0 +1,19 @@
+package com.example.clientfx.Controllers;
+
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class ErrorWindow {
+    public void showErrorWindow() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Connection Error");
+        alert.setHeaderText("Connection Error");
+        alert.setContentText("An error has occurred during the connection with the server");
+        ImageView icon = new ImageView(new Image("/com/example/clientfx/ErrorImage.png"));
+        icon.setFitHeight(50);
+        icon.setFitWidth(50);
+        alert.setGraphic(icon);
+        alert.showAndWait();
+    }
+}
--- src/main/java/com/example/clientfx/Controllers/Scena0Controller.java
+++ src/main/java/com/example/clientfx/Controllers/Scena0Controller.java
@@ -3,20 +3,50 @@ package com.example.clientfx.Controllers;
 import com.example.clientfx.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Scena0Controller {
+    public Label errorLabel;
     @FXML
     private TextField ipField;
     @FXML
     private TextField portField;
     private Main mainGui;
 
-
     public void start(ActionEvent event) throws Exception {
-        mainGui.setIp(ipField.getText());
-        mainGui.setPort(Integer.parseInt(portField.getText()));
-        mainGui.showScena1();
+        String ip=ipField.getText();
+        Integer port=null;
+        try {
+            port=Integer.parseInt(portField.getText());
+        }catch (NumberFormatException e) {
+            errorLabel.setVisible(true);
+            errorLabel.setText("Please enter a number");
+        }
+
+        if(tryConnection(ip,port)) {
+            mainGui.setIp(ip);
+            mainGui.setPort(port);
+            mainGui.showScena1();
+        }else {
+            ErrorWindow error= new ErrorWindow();
+            error.showErrorWindow();
+        }
+    }
+
+    private boolean tryConnection(String ip, Integer port) throws IOException {
+        Socket s;
+        try {
+            s=new Socket(ip,port);
+            s.close();
+            return true;
+        }catch ( IOException | NullPointerException | IllegalArgumentException e) {
+            return false;
+        }
+
     }
 
     public void setMainGui(Main mainGui) {
